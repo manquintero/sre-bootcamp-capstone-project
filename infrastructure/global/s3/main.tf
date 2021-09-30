90acfc6fb8dc59d88f08b929cb9268a4fcab9752
Antonio Quintero <man.quintero@gmail.com>
2021-09-29 23:46:07 -0500

    [infra] Remove credentiasl from secrets

--- infrastructure/global/s3/main.tf
+++ infrastructure/global/s3/main.tf
@@ -10,9 +10,7 @@ terraform {
 }
 
 provider "aws" {
-  access_key = "${var.aws_access_key}"
-  secret_key = "${var.aws_secret_key}"
-  region     = "${var.region}"
+  region = "us-east-2"
 }
 
 resource "aws_s3_bucket" "terraform_state" {
